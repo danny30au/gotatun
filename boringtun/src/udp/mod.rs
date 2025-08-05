7d002e3f33195ba446784162e64648d871f1d3b0
David Lönnhager <david.l@mullvad.net>
2025-08-05 12:15:30 +0200

    Implement AsFd on UDP socket wrapper

--- boringtun/src/udp/mod.rs
+++ boringtun/src/udp/mod.rs
@@ -3,6 +3,7 @@ use std::{
     future::Future,
     io,
     net::{Ipv4Addr, Ipv6Addr, SocketAddr},
+    os::fd::AsFd,
     sync::Arc,
 };
 
@@ -175,6 +176,12 @@ impl UdpSocket {
 
     pub fn local_addr(&self) -> io::Result<SocketAddr> {
         self.inner.local_addr()
+    }
+}
+
+impl AsFd for UdpSocket {
+    fn as_fd(&self) -> std::os::unix::prelude::BorrowedFd<'_> {
+        self.inner.as_fd()
     }
 }
 
